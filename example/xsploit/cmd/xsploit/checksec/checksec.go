20d2f5b12d2d410699d690b1db9982ee9afee1cf
Lei Wang <ssst0n3@gmail.com>
2023-11-16 17:30:30 +0800

    implement exmaple app xsploit's commands
    Signed-off-by: Lei Wang <ssst0n3@gmail.com>

---
+++ example/xsploit/cmd/xsploit/checksec/checksec.go
@@ -0,0 +1,14 @@
+package checksec
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+var Command = &cli.Command{
+	Name:    "checksec",
+	Aliases: []string{"c"},
+	Usage:   "check security inside a container",
+	Subcommands: []*cli.Command{
+		Auto,
+	},
+}
