c6a79fdd229e41ccbd1472a1afac613d92292c31
weinull <weinull@outlook.com>
2024-03-12 15:37:02 +0800

    update app env structs

--- pkg/env/app/basic.go
+++ pkg/env/app/basic.go
@@ -1,6 +1,16 @@
 package app
 
-type Env struct {
-	WebServerType WebServerType `json:type`
-	FrameworkType FrameworkType `json:type`
+type Basic struct {
+	WebServer WebServer `json:"web_server"`
+	Framework Framework `json:"framework"`
+}
+
+type WebServer struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type Framework struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
 }
--- pkg/env/app/env.go
+++ pkg/env/app/env.go
@@ -1,23 +1,6 @@
 package app
 
-type WebServerType int
-type FrameworkType int
-
-const (
-	WebServerForUnknown WebServerType = iota
-	WebServerForOther
-	WebServerForApache
-	WebServerForNginx
-	WebServerForTomcat
-	WebServerForIIS
-)
-
-const (
-	FrameworkForUnknown FrameworkType = iota
-	FrameworkForOther
-	FrameworkForJavaSpring
-	FrameworkForPyFlask
-	FrameworkForPyDjango
-	FrameworkForGoBeego
-	FrameworkForGoGin
-)
+type Env struct {
+	Basic
+	Advance
+}
