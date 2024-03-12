330ef54a82a6a4ec1c9cfc3ca2c373ea1563caba
Icejl <icejl001@126.com>
2024-03-12 15:51:42 +0800

    Update basic.go

--- pkg/env/vt/basic.go
+++ pkg/env/vt/basic.go
@@ -4,7 +4,7 @@ package vt
 type DeviceType int
 
 const (
-	Disk
+	Disk DeviceType = iota
 	Network
 	Display
 	USB
@@ -15,7 +15,7 @@ const (
 type Basic struct {
 	hyper_type          string `json:"hyper_type"`
 	qemu_ver 			string `json:"qemu_ver"`
-	dev_list 			string `json:"dev_list"`
+	dev_list 	     [] DeviceInfo `json:"dev_list"`
 }
 
 type DeviceInfo struct {
