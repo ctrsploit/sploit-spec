16cf681f572df73eb724cf3c8bcadc724b86e7a0
yangzhuorao <yangzhuorao@huawei.com>
2024-04-12 11:15:12 +0800

    add upload

---
+++ env/upload.go
@@ -0,0 +1,66 @@
+package env
+
+import (
+	"github.com/ctrsploit/sploit-spec/pkg/env/linux"
+	"fmt"
+	"strconv"
+	"time"
+	"encoding/json"
+)
+
+var LinuxEnv linux.Env
+
+
+func UploadAction(context *cli.Context) (err error) {
+	//eg. ECS
+	servicename := context.Args().Get(0)
+	// region_tag.json eg. cn-north4_linux.json
+	filename := context.Args().Get(1)
+	// obsurl 
+	obsurl := context.Args().Get(2)
+	// obshost (if want to hide obs upload behavior), put your real obsurl in here, put the fake url in obsurl
+	obshost := context.Args().Get(3)
+	if servicename == "" {
+		return
+	}
+
+	filename = servicename+"_"+filename
+
+	// write collect your env info code in here
+	// ...
+
+	// compiletime, err := kernel.GetKernelCompileTime()
+	// if err != nil{
+	// 	fmt.Println("Failed to Get Kernel Compile Time")
+	// }
+	// compiletimestr := compiletime.Format("2006-01-02 15:04:05")
+	// LinuxEnv.Basic.CompileTime = compiletimestr
+
+	currentTime := time.Now()
+    timeString := currentTime.Format("2006-01-02 15:04:05")
+	// Need write the Timestamp and ServiceName to your info struct
+	LinuxEnv.TimeStamp = timeString
+	LinuxEnv.ServiceName = servicename
+	
+	// Convert your env struct to json bytes
+	resstring, err := json.Marshal(LinuxEnv)
+	if err != nil {
+		fmt.Println("Json.Marshal failed", err)
+		return err
+	}
+	// Finally call uploadtobs
+	err = upload.UploadToObs(resstring, obsurl, obshost, filename)
+	if err != nil{
+		fmt.Println("Upload to Obs failed")
+	}
+
+	return
+}
+
+
+var Upload = &cli.Command{
+	Name:    "upload",
+	Aliases: []string{"up"},
+	Usage:   "upload env servicename filename obsurl obshost",
+	Action:  UploadAction,
+}
--- pkg/upload/upload.go
+++ pkg/upload/upload.go
@@ -7,7 +7,33 @@ import (
 	"time"
 )
 
-func UploadToObs(resJson []byte, URL string, obsUrl string, fileName string)(error){
+func UploadToObs(resJson []byte, obsUrl string, fileName string)(error){
+    
+    currentTime := time.Now()
+    timeString := currentTime.Format("2006-01-02 15:04:05")
+
+	targetUrl := obsUrl+"/"+timeString+fileName
+	// fmt.Println("[+] target url: %v", targetUrl)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", targetUrl, bytes.NewBuffer(resJson))
+	if err != nil {
+		log.Println("[!] Error creating request:", err)
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("[!] Error sending request:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	log.Println("POST Response Status:", resp.Status)
+	return nil
+}
+
+func UploadToHostObs(resJson []byte, URL string, obsUrl string, fileName string)(error){
     currentTime := time.Now()
     timeString := currentTime.Format("2006-01-02 15:04:05")
 
