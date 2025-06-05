726b3963d020c0c6b937620d1564cd3bd4f428ed
LEI WANG <ssst0n3@gmail.com>
2025-06-05 15:09:17 +0800

    Vul2ExploitCmd, Vul2VulCmd: add a parameter checkBeforeExploit
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- pkg/app/vul.go
+++ pkg/app/vul.go
@@ -21,16 +21,18 @@ func Vul2ChecksecCmd(v vul.Vulnerability, alias []string, flags []cli.Flag) *cli
 	}
 }
 
-func Vul2ExploitCmd(v vul.Vulnerability, alias []string, flags []cli.Flag) *cli.Command {
+func Vul2ExploitCmd(v vul.Vulnerability, alias []string, flags []cli.Flag, checkBeforeExploit bool) *cli.Command {
 	return &cli.Command{
 		Name:    v.GetName(),
 		Aliases: alias,
 		Usage:   v.GetDescription(),
 		Flags:   flags,
 		Action: func(context *cli.Context) (err error) {
-			_, err = v.CheckSec(context)
-			if err != nil {
-				return
+			if checkBeforeExploit {
+				_, err = v.CheckSec(context)
+				if err != nil {
+					return
+				}
 			}
 			err = v.Exploit(context)
 			return
@@ -38,12 +40,12 @@ func Vul2ExploitCmd(v vul.Vulnerability, alias []string, flags []cli.Flag) *cli.
 	}
 }
 
-func Vul2VulCmd(v vul.Vulnerability, alias []string, flagsCheckSec []cli.Flag, flagsExploit []cli.Flag) *cli.Command {
+func Vul2VulCmd(v vul.Vulnerability, alias []string, flagsCheckSec []cli.Flag, flagsExploit []cli.Flag, checkBeforeExploit bool) *cli.Command {
 	checksec := Vul2ChecksecCmd(v, []string{"c"}, flagsCheckSec)
 	checksec.Name = "checksec"
 	checksec.Usage = "check vulnerability exists"
 
-	exploit := Vul2ExploitCmd(v, []string{"x"}, flagsExploit)
+	exploit := Vul2ExploitCmd(v, []string{"x"}, flagsExploit, checkBeforeExploit)
 	exploit.Name = "exploit"
 	exploit.Usage = "run exploit"
 	return &cli.Command{
